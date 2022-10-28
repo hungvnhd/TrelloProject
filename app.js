c9f2b65a1d213564183bb5937f58d68f03140608
MrBien0405 <tranbien1845@gmail.com>
2022-10-28 18:02:28 +0700

    hahahahaha conflict roi nhe

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.use(express.static("public"));
 app.use(cookieParser("secret"));
 
 app.get("/", (req, res) => {
-  console.log("Login");
   res.send("hello");
 });
 app.use("/auth", authRoutes);
