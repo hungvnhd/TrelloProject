9336ec51e26e59e534b9350104b8601e65fff643
MrBien0405 <tranbien1845@gmail.com>
2022-11-03 09:23:48 +0700

    bien

--- public/register.js
+++ public/register.js
@@ -3,13 +3,9 @@ let registerForm = document.getElementById("register-form");
 const confirmInputPassword =
   document.getElementsByClassName("confirmPassword")[0];
 const inputUrl = document.getElementsByClassName("url")[0];
-<<<<<<< HEAD
 const inputEmail = document.getElementsByClassName("email")[0];
 const inputPassword = document.getElementsByClassName("password")[0];
-=======
-const inputEmail = document.getElementsByClassName("email")[0]
-const inputPassword = document.getElementsByClassName("password")[0]
->>>>>>> 16f42fc399a58e486e9fe59c1c167578326a2aab
+
 const showMessage = (message) => {
   let messageContainer = document.getElementsByClassName("message")[0];
   messageContainer.innerHTML = `<div class="alert alert-danger">${message}</div>`;
